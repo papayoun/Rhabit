feec3b0b9a7400d4b01c2dcbc173032571e6e594
TheoMichelot <theo.michelot@gmail.com>
2018-11-09 15:20:17 +0000

    add rhabitToRaster function

---
+++ R/rhabitToRaster.R
@@ -0,0 +1,14 @@
+
+#' Rhabit to raster
+#' 
+#' Transform a (x,y,z) list, as used in Rhabit, into a raster object
+#' 
+#' @param r List of three elements: x (grid of x values), y (grid of y values),
+#' and z (matrix of values)
+#' 
+#' @return Raster object
+#' 
+#' @export
+rhabitToRaster <- function(r) {
+  rasterFromXYZ(rasterToGGplot(r))
+}
